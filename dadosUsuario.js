06fdaa29230bbea4322e5a968c10f8239697aaf6
Douglasmd <165831836+Douglasmd@users.noreply.github.com>
2024-04-02 16:32:49 -0300

    Add files via upload

---
+++ dadosUsuario.js
@@ -0,0 +1,12 @@
+const userData = {
+    name: "markinhos",
+    profilePicture: "https://example.com/profile.jpg",
+    bio: "gosto de pular de costas",
+    location: "Cancão",
+    birthday: "01/01/4500 AC",
+    site: "https://maçom.com/",
+    joinDate: "Junho, 2015",
+    sexo: "menininha",
+    corfav: "Cor Favorita:roxo",
+    frasefav: "tão feliz que as vezes dá vontade de morrer rindo!!!!!",
+}
---
+++ jooge.js
@@ -0,0 +1,24 @@
+lugarDeBotarONome = document.getElementById("nomeDoSujeito");
+lugarDeBotarONome.innerHTML = userData.name;
+
+lugarDeBotarOlugar = document.getElementById("nomeDoLugar");
+lugarDeBotarOlugar.innerHTML = userData.location;
+
+lugarDeBotarOaniversario = document.getElementById("dataDoAniversario");
+lugarDeBotarOaniversario.innerHTML = userData.birthday;
+
+lugarDeBotarabio = document.getElementById("Bio");
+lugarDeBotarabio.innerHTML = userData.bio;
+
+lugarDeBotarOSexo = document.getElementById("Sexo");
+lugarDeBotarOSexo.innerHTML = userData.sexo;
+
+lugarDeBotarOSite = document.getElementById("Site");
+lugarDeBotarOSite.innerHTML = userData.site;
+
+lugarDeBotarAcor = document.getElementById("Cor");
+lugarDeBotarAcor.innerHTML = userData.corfav;
+
+lugarDeBotarFrase = document.getElementById("Frasefav");
+lugarDeBotarFrase.innerHTML = userData.frasefav;
+
